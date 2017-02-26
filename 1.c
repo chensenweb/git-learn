fba4b334806325eb05db198dc27c57baa5dc512e
chensengit <chensen@qq.com>
2017-02-26 12:40:05 +0800

    bulid 1.c file

--- 1.c
+++ 1.c
@@ -1,19 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define LEN 100 
+
 int main()
 {
-    char str[LEN];
-    int i=0,j=0;
-    fgets(str,LEN,stdin);
-    for(;str[i]!='\0';i++)
-    {
-        if(str[i]!=' ')
-        {
-            str[j++]=str[i];
-            
-        }
-    }
-    str[j]='\0';
-    printf("%s\n",str);
+    int num=10;
+    printf("%d",num);
 }
